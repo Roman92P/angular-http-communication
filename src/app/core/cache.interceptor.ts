1b66016fa0e9a529b20e223c54ad23687305dd04
Roman92P <roman.paszkow@gmail.com>
2022-10-14 14:45:43 +0200

    adding cache service

---
+++ src/app/core/cache.interceptor.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+} from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { tap } from "rxjs/operators";
+import { HttpCacheService } from "./http-cache.service";
+import { ReturnStatement } from "@angular/compiler";
+
+@Injectable()
+export class CacheInterceptor implements HttpInterceptor {
+  constructor(private cacheService: HttpCacheService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (req.method !== "GET") {
+      console.log(`Invalidating cache: ${req.method}`);
+      this.cacheService.invalidateCache();
+      return next.handle(req);
+    }
+
+    const cacheResponse: HttpResponse<any> = this.cacheService.get(req.url);
+
+    if (cacheResponse) {
+      console.log(`Returning a cached response: ${cacheResponse.url}`);
+      console.log(cacheResponse);
+      return of(cacheResponse);
+    }
+
+    return next.handle(req).pipe(
+      tap((event) => {
+        if (event instanceof HttpResponse) {
+          console.log(`Adding item to cache: ${req.url}`);
+          this.cacheService.put(req.url, event);
+        }
+      })
+    );
+  }
+}
